e804f6811a9e5e95696a26e570f4bc352cc8a182
Kalayi <115426911+Kalayi0@users.noreply.github.com>
2022-11-14 14:36:14 +0000

    Delete app directory

--- app/utils/helpers/index.js
+++
@@ -1,6 +0,0 @@
-import AuthHelper from './auth';
-import GenericHelper from './generic';
-import ValidationHelper from './validations';
-import ErrorFactory from './efactory';
-
-export { AuthHelper, GenericHelper, ValidationHelper, ErrorFactory };
