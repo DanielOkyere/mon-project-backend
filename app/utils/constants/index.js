7976b7b4b01f45a48e08e9fef8ae2cc58eac3d39
Kalayi <115426911+Kalayi0@users.noreply.github.com>
2022-11-19 19:14:01 +0000

    Delete app directory

--- app/utils/constants/index.js
+++
@@ -1,9 +0,0 @@
-import apiMessage from './api.message';
-import dbUnique from './unique.constraints';
-import eventsConstants from './events.constants';
-
-export default {
-  ...apiMessage,
-  ...dbUnique,
-  ...eventsConstants
-};
